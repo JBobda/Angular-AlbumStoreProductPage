573dee3673cf2f5c8d202847d84285bb45ab789f
JBobda <J.BobdaT@gmail.com>
2019-01-04 12:29:14 -0600

    Made the album url private

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,11 +2,10 @@ import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/map';
 import { Http, Response } from '@angular/http';
-import { mapChildrenIntoArray } from '@angular/router/src/url_tree';
 
 @Injectable()
 export class ProductService {
-  _albumUrl = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
